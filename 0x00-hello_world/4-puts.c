a2deb066fcf916b01a6bf8f8ae681ca50d2f3af6
Aymane Chaouki <aymane.chdev@gmail.com>
2023-10-05 18:02:48 -0700

    hhhh

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - Entry point of the program
+*
+* Description: print string in c usin puts folowed by new line .
+* Return: the value 0.
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
