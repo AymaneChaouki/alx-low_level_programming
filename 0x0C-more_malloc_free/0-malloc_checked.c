ae3e87222f0239cdf845aeb1fafaf7f1f222703a
Aymane Chaouki <aymane.chdev@gmail.com>
2023-11-02 13:32:09 -0700

    init

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * malloc_checked - The Function
+ * @b:  The variable
+ * Return: The Return value/void
+*/
+
+void	*malloc_checked(unsigned int b)
+{
+	void	*ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
