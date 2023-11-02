596c4313e747d262d7dcb5a4a5aae8d8fbec3a2a
Aymane Chaouki <aymane.chdev@gmail.com>
2023-11-02 13:32:40 -0700

    init

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strlen - The Function
+ * @s:  The variable
+ * Return: The Return value/void
+ */
+unsigned int	_strlen(char *s)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * string_nconcat - The Function
+ * @s1: The variable
+ * @s2: The variable
+ * @n: The variable
+ * Return: The Return value/void
+*/
+
+char	*string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int	i;
+	unsigned int	j;
+	unsigned int	len1;
+	unsigned int	len2;
+	char			*concat;
+
+	len1 = 0;
+	len2 = 0;
+	if (!s1)
+		s1 = "";
+	if (!s2)
+		s2 = "";
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	if (n >= len2)
+		n = len2;
+	concat = malloc(sizeof(char) * (len1 + n + 1));
+	if (!concat)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		concat[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j] && j < n)
+	{
+		concat[i + j] = s2[j];
+		j++;
+	}
+	concat[i + j] = '\0';
+	return (concat);
+}
