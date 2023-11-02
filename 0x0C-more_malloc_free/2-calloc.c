bb3e15f21c1f626813411bb1c52879348636fbe5
Aymane Chaouki <aymane.chdev@gmail.com>
2023-11-02 13:33:50 -0700

    init

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _calloc - The Function
+ * @nmemb: The variable
+ * @size: The variable
+ * Return: The Return value/void
+*/
+
+void	*_calloc(unsigned int nmemb, unsigned int size)
+{
+	char			*ptr;
+	unsigned int	i;
+	unsigned int	total_size;
+
+	i = 0;
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	total_size = nmemb * size;
+	ptr = malloc(total_size);
+	if (!ptr)
+		return (NULL);
+	while (i < total_size)
+	{
+		ptr[i] = 0;
+		i++;
+	}
+	return (ptr);
+}
