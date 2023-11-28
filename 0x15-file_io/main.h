d4f432bf829839609711757b4a1d1ea4430bf1d4
Aymane Chaouki <aymane.chdev@gmail.com>
2023-11-28 02:14:45 -0800

    init

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,7 +3,7 @@
 
 /*
  * File: holberton.h
- * Auth: Godswill Kalu
+ * Auth: aychaouk
  * Desc: Header file containing prototypes for all functions
  *       written in the 0x14-file_io directory.
  */
@@ -15,13 +15,12 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-
 /**
  * ELF header functions
+ * e_ident : is bullshit
 */
 void check_elf(unsigned char *e_ident);
 void print_magic(unsigned char *e_ident);
